e671d6367aecad9c6b086e7a6c5bde4e8d5c6ee5
Stanimir Kovachev <stanimir.kovachevv@gmail.com>
2021-12-22 14:06:15 +0200

    Basic elements added/

---
+++ src/main/java/com/projects/liftingvault/LiftingVaultApplication.java
@@ -0,0 +1,13 @@
+package com.projects.liftingvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftingVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiftingVaultApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projects/liftingvault/LiftingVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.liftingvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftingVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
